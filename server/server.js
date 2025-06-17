5ae6a0ac2ec885174a9257abba6bcf155244f5b3
Anunay Nayan <anunayan123@gmail.com>
2025-06-17 15:58:24 +0530

    Frontend URL update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://taskverse-phi.vercel.app/'], 
+  origin: ['http://localhost:5173', 'https://taskverse-phi.vercel.app'], 
   credentials: true,
 }));
 app.use(express.json());
